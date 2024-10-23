ee7ed3affe4f3bf1a27d785575ce71912f3e5a2e
Andrey Tutolmin <tutolmin@gmail.com>
2024-10-23 16:45:15 +0300

    new tutolmin v24 keymap

---
+++ keyboards/zsa/moonlander/keymaps/tutolmin/config.h
@@ -0,0 +1,15 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#undef ONESHOT_TIMEOUT
+#define ONESHOT_TIMEOUT 2000
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define SERIAL_NUMBER "BwpKd/ZYX7z"
+#define LAYER_STATE_8BIT
+#define COMBO_COUNT 7
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
